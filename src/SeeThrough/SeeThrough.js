cfe34435707975eb6177991705f9757c5e5a40e6
Andrew Guterman <andrew.guterman1@gmail.com>
2020-04-02 23:10:04 -0700

    clean up unused useMemo

--- src/SeeThrough/SeeThrough.js
+++ src/SeeThrough/SeeThrough.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import PartialMask from './PartialMask';
 import { withResizeDetector } from 'react-resize-detector';
